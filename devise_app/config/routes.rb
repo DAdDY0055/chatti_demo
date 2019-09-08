29c0c9c7f201e81a9e6862344237a7db2b1fc951
Takuya <yky2800.rthm.55tn@gmail.com>
2019-09-08 20:20:04 +0900

    devise導入

--- devise_app/config/routes.rb
+++ devise_app/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'blogs#index'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # devise
+  root to: "home#index"
 end
